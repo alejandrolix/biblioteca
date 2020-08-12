4471bc5ce1168d09b814f68379841911c7c9c780
Alejandro Pons <alejandro.pons98@gmail.com>
2020-08-12 14:27:30 +0200

    Creación libros en JavaScript

---
+++ prueba.js
@@ -0,0 +1,6 @@
+const utilidades = require(__dirname + '/utilidades.js');  // O usar ./utilidades.js para incluír el fichero en la ruta actual.
+
+console.log('hola mundo');
+// console.log(__dirname);
+console.log(utilidades.sumar(1, 1));
+console.log(utilidades.restar(1, 1));
---
+++ utilidades.js
@@ -0,0 +1,6 @@
+console.log('Entrando en utilidades.js');
+
+module.exports = {
+    sumar: (num1, num2) => num1 + num2,
+    restar: (num1, num2) => num1 - num2
+};
