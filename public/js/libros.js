ffe47e8c92bda9c2252208cb5dc67bf61e77a466
Alejandro Pons <alejandro.pons98@gmail.com>
2020-08-13 12:11:57 +0200

    Cambiado forma de llamar a la función de obtener los libros por AJAX

--- public/js/libros.js
+++ public/js/libros.js
@@ -1,111 +1,63 @@
-// function getLibros() {
-//     let libros = {
-//       libros: [
-//           {
-//               id: 1,
-//               titulo: 'El capitán Alatriste',
-//               autor: {
-//                   id: 2,
-//                   nombre: 'Arturo Pérez Reverte'
-//               },
-//               isbn: 'cscscscsmmofsdvsd',
-//               precio: 25.50,
-//               activo: true,
-//               imagen: 'segway1.jpg'
-//           },
-//           {
-//               id: 2,
-//               titulo: 'El Nombre de la Rosa',
-//               autor: {
-//                   id: 3,
-//                   nombre: 'Umberto Eco'
-//               },
-//               isbn: '444A',
-//               precio: 45,
-//               activo: true,
-//               imagen: 'segway1.jpg'
-//           },
-//           {
-//               id: 3,
-//               titulo: 'El Señor de los Anillos',
-//               autor: {
-//                   id: 4,
-//                   nombre: 'Tolkin'
-//               },
-//               isbn: '555A',
-//               precio: 70,
-//               activo: true,
-//               imagen: 'segway1.jpg'
-//           },
-//           {
-//               id: 3,
-//               titulo: 'Los Pilares de la tierra',
-//               autor: {
-//                   id: 4,
-//                   nombre: 'Ken Follet'
-//               },
-//               isbn: '555A',
-//               precio: 70,
-//               activo: false,
-//               imagen: 'segway1.jpg'
-//           }
-//       ]
-//     };
-//
-//     return libros;
-// }
-
-window.addEventListener('load', () => {
-    // let libros = getLibros();
-
+function getLibros() {
     let http = new XMLHttpRequest();
     http.open('GET', 'http://localhost:8080/libros', true);
     http.addEventListener('readystatechange', () => {
 
-       if (http.readyState == 4 && http.status == 200) {
-           let libros = JSON.parse(http.responseText);
+        if (http.readyState == 4 && http.status == 200) {
+            let resultado = JSON.parse(http.responseText);
 
-           let divLibros = document.getElementById('libros');
-           let cadenaLibros = '';
-
-           for (let i = 0; i < libros.data.length; i++) {
-               let libro = libros.data[i];
-
-               if (libro.activo) {
-                   let precio = libro.precio.toLocaleString('es-ES', {
-                       style: 'currency',
-                       currency: 'EUR'
-                   });
-
-                   cadenaLibros = cadenaLibros + '<div class="col-md-4">\n' +
-                       '                            <div class="card mb-4 shadow-sm">\n' +
-                       '                                <img src="imagenes/' + libro.imagen + '" class="img-fluid" alt="Segway">\n' +
-                       '                                <div class="card-body">\n' +
-                       '                                    <p class="card-text">' + libro.titulo + '</p>\n' +
-                       '                                    <p class="card-text">' + libro.autor.nombre + '</p>\n' +
-                       '                                    <div class="d-flex justify-content-between align-items-center">\n' +
-                       '                                        <div class="btn-group">\n' +
-                       '                                            <button type="button" class="btn btn-sm btn-outline-secondary" data-toggle="modal" data-target="#verLibro">\n' +
-                       '                                                <i class="fas fa-eye"></i>\n' +
-                       '                                            </button>\n' +
-                       '                                            <a href="formularioLibro.html" class="btn btn-sm btn-outline-secondary">\n' +
-                       '                                                <i class="fas fa-edit"></i>\n' +
-                       '                                            </a>\n' +
-                       '                                            <a href="eliminar.html" class="btn btn-sm btn-outline-secondary">\n' +
-                       '                                                <i class="fas fa-trash"></i>\n' +
-                       '                                            </a>\n' +
-                       '                                        </div>\n' +
-                       '                                        <small class="text-muted">' + precio + '</small>\n' +
-                       '                                    </div>\n' +
-                       '                                </div>\n' +
-                       '                            </div>\n' +
-                       '                        </div>';
-               }
-           }
-
-           divLibros.innerHTML = cadenaLibros;
-       }
+            if (resultado.ok) {
+                let libros = resultado.data;
+                mostrarLibros(libros);
+            }
+        }
     });
 
     http.send();
+}
+
+window.addEventListener('load', () => {
+    getLibros();
 });
+
+function mostrarLibros(libros) {
+    let divLibros = document.getElementById('libros');
+    let cadenaLibros = '';
+
+    for (let i = 0; i < libros.length; i++) {
+        let libro = libros[i];
+
+        if (libro.activo) {
+            let precio = libro.precio.toLocaleString('es-ES', {
+                style: 'currency',
+                currency: 'EUR'
+            });
+
+            cadenaLibros = cadenaLibros + '<div class="col-md-4">\n' +
+                '                            <div class="card mb-4 shadow-sm">\n' +
+                '                                <img src="imagenes/' + libro.imagen + '" class="img-fluid" alt="Segway">\n' +
+                '                                <div class="card-body">\n' +
+                '                                    <p class="card-text">' + libro.titulo + '</p>\n' +
+                '                                    <p class="card-text">' + libro.autor.nombre + '</p>\n' +
+                '                                    <div class="d-flex justify-content-between align-items-center">\n' +
+                '                                        <div class="btn-group">\n' +
+                '                                            <button type="button" class="btn btn-sm btn-outline-secondary" data-toggle="modal" data-target="#verLibro">\n' +
+                '                                                <i class="fas fa-eye"></i>\n' +
+                '                                            </button>\n' +
+                '                                            <a href="formularioLibro.html" class="btn btn-sm btn-outline-secondary">\n' +
+                '                                                <i class="fas fa-edit"></i>\n' +
+                '                                            </a>\n' +
+                '                                            <a href="eliminar.html" class="btn btn-sm btn-outline-secondary">\n' +
+                '                                                <i class="fas fa-trash"></i>\n' +
+                '                                            </a>\n' +
+                '                                        </div>\n' +
+                '                                        <small class="text-muted">' + precio + '</small>\n' +
+                '                                    </div>\n' +
+                '                                </div>\n' +
+                '                            </div>\n' +
+                '                        </div>';
+        }
+    }
+
+    divLibros.innerHTML = cadenaLibros;
+}
