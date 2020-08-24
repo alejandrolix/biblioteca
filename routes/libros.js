0725b4edaa484b17039f2ad90f0ab7820ddc28b1
alejandro <alejandro.pons98@gmail.com>
2020-08-24 20:09:19 +0200

    Cambiado mostrar el autor en lugar de null si el libro no tiene autor

--- routes/libros.js
+++ routes/libros.js
@@ -5,7 +5,10 @@ const Imagen = require("../utilidades/imagen");
 let router = express.Router();
 
 router.get('/', (req, res) => {
-    let sql = `select l.cod, l.titulo, l.precio, l.imagen, l.activo, a.nombre as autor 
+    let sql = `select l.cod, l.titulo, l.precio, l.imagen, l.activo, case
+                                                                        when a.nombre is null then 'Anónimo'
+                                                                        when a.nombre is not null then a.nombre
+                                                                     end as autor 
                from libros l 
                     left join autor a on a.cod = l.cod_autor `;
 
@@ -67,7 +70,10 @@ router.get('/', (req, res) => {
 });
 
 router.get('/activos', (req, res) => {
-    let sql = `select l.cod, l.titulo, l.precio, l.imagen, a.nombre as autor 
+    let sql = `select l.cod, l.titulo, l.precio, l.imagen, case
+                                                                when a.nombre is null then 'Anónimo'
+                                                                when a.nombre is not null then a.nombre
+                                                           end as autor 
                from libros l 
                     left join autor a on a.cod = l.cod_autor 
                where l.activo = true      
@@ -110,7 +116,10 @@ router.get('/activos', (req, res) => {
 });
 
 router.get('/noActivos', (req, res) => {
-    let sql = `select l.cod, l.titulo, l.precio, l.imagen, a.nombre as autor 
+    let sql = `select l.cod, l.titulo, l.precio, l.imagen, case
+                                                                when a.nombre is null then 'Anónimo'
+                                                                when a.nombre is not null then a.nombre
+                                                           end as autor 
                from libros l 
                     left join autor a on a.cod = l.cod_autor 
                where l.activo = false      
@@ -154,7 +163,10 @@ router.get('/noActivos', (req, res) => {
 
 router.get('/:id', (req, res) => {
     let idLibro = req.params.id;
-    let sql = `select l.cod, l.titulo, l.isbn, l.precio, l.imagen, l.url, l.activo, a.nombre as autor 
+    let sql = `select l.cod, l.titulo, l.isbn, l.precio, l.imagen, l.url, l.activo, case
+                                                                                        when a.nombre is null then 'Anónimo'
+                                                                                        when a.nombre is not null then a.nombre
+                                                                                    end as autor 
                from libros l 
                     left join autor a on a.cod = l.cod_autor 
                where l.cod = ?;`;
